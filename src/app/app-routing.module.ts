2954165644214f599e83dc59659b323c63f2592d
omkhambhata24 <omkhambhata@gmail.com>
2022-02-02 14:58:36 +0530

    created Reactive Form component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChildComponent } from './parent/child/child.component';
 import { ParentComponent } from './parent/parent.component';
 import { PipesComponent } from './pipes/pipes.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 const routes: Routes = [
    { path: 'pipes', component: PipesComponent},
-   { path: 'Parent-child', component: ParentComponent}
+   { path: 'Parent-child', component: ParentComponent},
+   { path: 'Form', component: ReactiveFormComponent}
 
 ];
 
