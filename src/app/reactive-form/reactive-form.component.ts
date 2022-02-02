2eaeebb0356462b41411a79533e3e48d7cf29442
Omkhambhata24 <omkhambhata@gmail.com>
2022-02-02 23:09:48 +0530

    validations of form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormBuilder, Validators} from '@angular/forms';
+import { FormControl, FormBuilder, Validators, FormGroup, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -8,24 +8,92 @@ import { FormControl, FormBuilder, Validators} from '@angular/forms';
 })
 export class ReactiveFormComponent implements OnInit {
 
-  resume = this.fb.group({
+  resume: FormGroup;
+  Skills: FormArray;
+  Experience: FormArray;
+  Education: FormArray;
 
-    Name: ['',],
-    Skills: ['',],
-    Experience: ['',],
-    Education: ['',],
+  buildForm(): void {
+    this.resume = this.fb.group({
+
+    Name: ['',[Validators.required, Validators.minLength(4), Validators.maxLength(9)]],
+    Skills: this.fb.array([
+      this.dynamicField()
+    ]),
+
+    Experience: this.fb.array([
+      this.dynamicField()
+    ]),
+
+    Education: this.fb.array([
+      this.dynamicField()
+    ]),
+
     Email: ['', [Validators.email, Validators.required]],
     Mobile: ['', Validators.required],
 
   });
 
-  get name(): any {
-    return this.resume.get('name');
+
+
+  
 }
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.buildForm();
+  }
+
+  dynamicField(): FormGroup {
+    return this.fb.group({
+      Skills: [''],
+      Education: [''],
+      Experience:['']
+    })
+  }
+
+  addSkills() {
+    this.Skills = this.resume.get('skills') as FormArray;
+    this.Skills.push(this.dynamicField())
+  }
+
+  addEducation() {
+    this.Education = this.resume.get('Education') as FormArray;
+    this.Education.push(this.dynamicField())
+  }
+
+  addExperience() {
+    this.Experience = this.resume.get('Experience') as FormArray;
+    this.Experience.push(this.dynamicField())
+  }
+
+  saveDetails() {
+    console.log(this.resume.value)
+  }
+
+  deleteSkills(index:number){
+    if(this.Skills.length !=1){
+      this.Skills = this.resume.get('Skills') as FormArray;
+      this.Skills.removeAt(index)
+    }
+    console.log(this.Skills.length)
+  }
+
+  deleteExperience(index:number){
+    if(this.Experience.length !=1){
+      this.Experience = this.resume.get('Experience') as FormArray;
+      this.Experience.removeAt(index)
+    }
+    console.log(this.Experience.length)
+  }
+
+  deleteEducation(index:number){
+    if(this.Education.length !=1){
+      this.Education = this.resume.get('Skills') as FormArray;
+      this.Education.removeAt(index)
+    }
+    console.log(this.Education.length)
   }
 
 }
