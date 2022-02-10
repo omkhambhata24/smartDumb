7381b7ad71c68e9ce7f0172a8679f8be205f4a5d
omkhambhata24 <omkhambhata@gmail.com>
2022-02-10 19:57:19 +0530

    fix: Gave styling to profile list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './parent/child/child.component';
-import { PipesComponent } from './pipes/pipes.component';
 import { PipesModule } from './pipes/pipes.module';
 import { CoreModule } from './core/core.module';
 import { HeaderComponent } from './core/header/header.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProfileModule } from './profile/profile.module';
 
 
