824881b370ede89543d1c30bcc67f72e731b4289
omkhambhata24 <omkhambhata@gmail.com>
2022-06-03 18:05:22 +0530

    Feat: Stepper form Completed.

--- src/app/progress-form-container/progress-form-presenter/progress-form-presenter.service.ts
+++ src/app/progress-form-container/progress-form-presenter/progress-form-presenter.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProgressFormPresenterService {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+//   personalDetails = this.formBuilder.group({
+//     name: ['', Validators.required],
+//     email: ['', Validators.required],
+//     phone: ['', Validators.required]
+//   });
+
+// addressDetails = this.formBuilder.group({
+//   city: ['', Validators.required],
+//   address: ['', Validators.required],
+//   pincode: ['', Validators.required]
+// });
+
+// educationalDetails = this.formBuilder.group({
+//   highest_qualification: ['', Validators.required],
+//   university: ['', Validators.required],
+//   total_marks: ['', Validators.required]
+// });
 }
